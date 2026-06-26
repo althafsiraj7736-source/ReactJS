0eb83567e7309b77915b24b8cd764154d8b1a436
Althaf s <althafs879@gmail.com>
2026-06-26 08:49:27 +0530

    Project UI

---
+++ 07-ui-project/src/components/section1/Arrrow.jsx
@@ -0,0 +1,14 @@
+// import React from 'react'
+import { RiArrowRightUpLine } from "@remixicon/react";
+
+const Arrrow = () => {
+  return (
+    <div>
+        <div className="text-8xl py-12">
+            <RiArrowRightUpLine className="text-black size-20 cursor-pointer" />
+        </div>
+    </div>
+  )
+}
+
+export default Arrrow
---
+++ 07-ui-project/src/components/section1/HeroText.jsx
@@ -0,0 +1,12 @@
+// import React from 'react'
+
+const HeroText = () => {
+  return (
+    <div className="p-5">
+            <h3 className="mb-7 leading-[1.1] text-4xl font-bold">Prospective <br /><span className="text-gray-700">Customer</span> Segmentation</h3>
+            <p className="text-xl font-medium text-gray-600">Discover and target your most valuable customers with precision analytics and strategic segmentation. This helps businesses identify and categorize potential customers based on demographics, interests, behaviors, and purchasing intent. </p>
+        </div>
+  )
+}
+
+export default HeroText
